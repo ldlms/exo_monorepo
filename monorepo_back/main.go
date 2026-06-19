499d36f3d23da3669e590035ff8d9698de2cb74d
ldlms <leodelmas31@gmail.com>
2026-06-19 09:55:44 +0200

    linter

--- monorepo_back/main.go
+++ monorepo_back/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "log"
+
+    "github.com/gin-gonic/gin"
+)
 
 func main() {
     r := gin.Default()
     r.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{"message": "Hello world"})
     })
-    r.Run() 
+    if err := r.Run(); err != nil {
+        log.Fatalf("erreur au démarrage du serveur: %v", err)
+    }
 }
